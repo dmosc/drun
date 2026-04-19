0b7409ec381d6213b89930162fa433f42c44b723
dmosc <osdavrm@gmail.com>
2026-04-18 18:45:39 -0700

    Implement DrunEngine.
    We can now process any arbitrary piece of Python code using the WASM compiled Python interpreter.

--- crates/drun-core/src/lib.rs
+++ crates/drun-core/src/lib.rs
@@ -1,3 +1,54 @@
-pub fn version() -> &'static str {
-    "0.1.0"
+use std::io::{Read, Seek, SeekFrom};
+use wasmtime::{Config, Engine, Linker, Module, Store};
+use wasmtime_wasi::{WasiCtxBuilder, cli::OutputFile, p1::WasiP1Ctx};
+
+pub struct DrunEngine {
+    engine: Engine,
+    linker: Linker<WasiP1Ctx>,
+    module: Module,
+}
+
+impl DrunEngine {
+    pub fn new() -> anyhow::Result<Self> {
+        let config = Config::new();
+        let engine = Engine::new(&config)?;
+        let mut linker = Linker::new(&engine);
+        wasmtime_wasi::p1::add_to_linker_sync(&mut linker, |t| t)?;
+        let wasm_bytes = include_bytes!("assets/python-3.12.0.wasm");
+        let module = Module::from_binary(&engine, wasm_bytes)?;
+
+        Ok(Self {
+            engine,
+            linker,
+            module,
+        })
+    }
+
+    pub fn run_python(&self, code: &str) -> anyhow::Result<String> {
+        // Initialize temp files to store runtime output.
+        let stdout_temp = tempfile::tempfile()?;
+        let stderr_temp = tempfile::tempfile()?;
+        // Initialize a WASM runtime.
+        let mut wasi_builder = WasiCtxBuilder::new();
+        wasi_builder
+            .stdout(OutputFile::new(stdout_temp.try_clone()?))
+            .stderr(OutputFile::new(stderr_temp.try_clone()?))
+            .args(&["python", "-c", code]);
+        let mut store = Store::new(&self.engine, wasi_builder.build_p1());
+        let instance = self.linker.instantiate(&mut store, &self.module)?;
+        let start = instance.get_typed_func::<(), ()>(&mut store, "_start")?;
+        // Execute code in WASM runtime.
+        if let Err(e) = start.call(&mut store, ()) {
+            if !e.to_string().contains("exit status 0") {
+                anyhow::bail!("Python error: {}", e);
+            }
+        }
+        // Store outputs in tempfiles.
+        let mut result = String::new();
+        let mut file = stdout_temp;
+        file.seek(SeekFrom::Start(0))?;
+        file.read_to_string(&mut result)?;
+
+        Ok(result)
+    }
 }
--- crates/drun-py/src/lib.rs
+++ crates/drun-py/src/lib.rs
@@ -1,12 +1,22 @@
-use pyo3::prelude::*;
+use drun_core::DrunEngine;
+use pyo3::{exceptions::PyRuntimeError, prelude::*};
+use std::sync::OnceLock;
+
+static ENGINE: OnceLock<DrunEngine> = OnceLock::new();
 
 #[pyfunction]
-fn get_version() -> String {
-    drun_core::version().to_string()
+fn execute(code: String) -> PyResult<String> {
+    let engine =
+        ENGINE.get_or_init(|| DrunEngine::new().expect("Failed to initialize DrunEngine."));
+
+    engine
+        .run_python(&code)
+        .map_err(|e| PyRuntimeError::new_err(e.to_string()))
 }
 
 #[pymodule]
 fn drun(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(get_version, m)?)?;
+    m.add_function(wrap_pyfunction!(execute, m)?)?;
+
     Ok(())
 }
